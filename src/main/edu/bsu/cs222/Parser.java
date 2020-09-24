aedeee1639e7745f291993cb1e14c079f38b8602
Michael Heckman <msheckman@bsu.edu>
2020-09-24 16:36:25 -0400

    Added convert(), search(), and connectToWikipedia in WebpageSearcher, added tests in WebpageSearcherTest, added test in ParserTest

---
+++ src/main/edu/bsu/cs222/Parser.java
@@ -0,0 +1,10 @@
+package edu.bsu.cs222;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class Parser {
+    public List<Revision> parse(InputStream input) {
+        return null;
+    }
+}
